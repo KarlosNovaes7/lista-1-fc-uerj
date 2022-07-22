84d6410cd49e87c8e4c5b9277df3c62db4adef63
Sabrina <helsabrinacarvalho@hotmail.com>
2022-07-22 18:22:07 -0300

    att

---
+++ ex-4.py
@@ -0,0 +1,17 @@
+
+nomeProduto = input('digite o nome do produto: ')
+produtoMaisCaro = 0
+nomeProdutoMaisCaro = ""
+precoProduto = 0
+
+while(nomeProduto != 'xxx'):
+    precoProduto = int(input('digite o preço do produto: '))
+    if (precoProduto > produtoMaisCaro):
+        produtoMaisCaro = precoProduto
+        nomeProdutoMaisCaro = nomeProduto
+    nomeProduto = input('digite o nome do produto: ')
+
+
+
+print("Nome do produto mais caro", nomeProdutoMaisCaro)
+print('Produto mais caro', produtoMaisCaro)
